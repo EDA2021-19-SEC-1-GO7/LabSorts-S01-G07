ff34bab12f9fe9e9d3e228d2b774339f8ca0acb4
martin-2k <85652994+martin-2k@users.noreply.github.com>
2021-06-22 16:51:47 -0500

    adadw

--- App/view.py
+++ App/view.py
@@ -80,6 +80,15 @@ def printBestBooks(books):
     else:
         print('No se encontraron libros')
 
+def printResults(ord_books, sample=10): 
+    size = lt.size(ord_books) 
+    if size > sample: 
+        print("Los primeros ", sample, " libros ordenados son:") 
+        i=1 
+        while i <= sample: 
+            book = lt.getElement(ord_books,i) 
+            print('Titulo: ' + book['title'] + ' ISBN: ' + book['isbn'] + ' Rating: ' + book['average_rating']) 
+            i+=1
 catalog = None
 
 """
@@ -113,11 +122,11 @@ while True:
         book_count = controller.countBooksByTag(catalog, label)
         print('Se encontraron: ', book_count, ' Libros')
 
-    elif int(inputs[0]) == 5:
-        size = input("Indique tamaño de la muestra: ")
-        result = controller.sortBooks(catalog, int(size))
-        print("Para la muestra de", size, " elementos, el tiempo (mseg) es: ",
-                                          str(result))
+    elif int(inputs[0]) == 5: 
+        size = input("Indique tamaño de la muestra: ") 
+        result = controller.sortBooks(catalog, int(size)) 
+        print("Para la muestra de", size, " elementos, el tiempo (mseg) es: ", str(result[0])) 
+        printResults(result[1])
 
     else:
         sys.exit(0)
